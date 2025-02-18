be114055a123a35a34513cce93f34e058a5f1d7a
spacewhale13 <alexandranikushina@gmail.com>
2025-02-18 21:20:00 +0300

    Добавлено изменение

--- homework_2/task_2.py
+++ homework_2/task_2.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """Задача 2. Строки: Срезы
 Описание: Дана строка. выведите с 4 по 10 символы, если это не возможно, дайте знать это"""
 
@@ -12,18 +11,3 @@ def print_string(line):
 
 
 print_string("ethnxrfmrmr,mr,mxu")
-=======
-"""Задача 2. Строки: Срезы
-Описание: Дана строка. выведите с 4 по 10 символы, если это не возможно, дайте знать это"""
-
-
-def print_string(line):
-    if len(line) >= 10:
-        print(line[4:11])
-    else:
-        print("No god")
-    return line
-
-
-print_string("ethnxrfmrmr,mr,mxu")
->>>>>>> de8b46c386c5496f5f8adeb5ee10c13a723663b9
--- homework_2/task_7.py
+++ homework_2/task_7.py
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-"""Задача 7. Переменные, списки: Распаковка последовательностей
-Описание: Создайте список из трёх элементов, распакуйте его в отдельные переменные и выведите каждую переменную."""
-
-
-def unpacking_elements(list_elements):
-
-    a, b, c = list_elements
-    print(a, b, c)
-    return a, b, c
-
-
-list_elements = [2, 3, 4]
-
-unpacking_elements(list_elements)
-=======
 """Задача 7. Переменные, списки: Распаковка последовательностей
 Описание: Создайте список из трёх элементов, распакуйте его в отдельные переменные и выведите каждую переменную."""
 
@@ -27,4 +11,3 @@ def unpacking_elements(list_elements):
 list_elements = [2, 3, 4]
 
 unpacking_elements(list_elements)
->>>>>>> de8b46c386c5496f5f8adeb5ee10c13a723663b9
