d261f920d57b4e7559f0b54255123355751901ba
spacewhale13 <alexandranikushina@gmail.com>
2025-03-01 09:46:09 +0300

    Добавлены исправления

--- Homework_3/binnary_search.py
+++ Homework_3/binnary_search.py
@@ -13,41 +13,14 @@ def binary_search(num, target):
 
         elif num[middle] == target:
 
-            return num[middle]
+            return True
 
-    return "Ошибка"
+    return False
 
 
-num = [1, 2, 3]
+num = [1, 2, 3, 4, 5, 6, 7]
 target = 2
-if binary_search(num, target) == "Ошибка":
-    print("Элемент не найден")
-else:
-    print(binary_search(num, target))
-
-
-"""Дан отсортированный список чисел и целевое число target. Тебе нужно найти число в списке, которое ближе всего к target."""
-
-
-def closet_search(num, target):
-    left = 0
-    right = len(num) - 1
-    closet = num[0]
-
-    while left <= right:
-
-        middle = (right + left) // 2
-        if (target - num[middle]) > (target - num[0]):
-            closet = num[middle]
-            if (target - num[middle]) == (target - closet):
-                return min(closet, num[middle])
-
-    return "Ошибка"
-
-
-num = [1, 2, 3, 16, 389, 489]
-target_1 = 4
-if closet_search(num, target) == "Ошибка":
+if binary_search(num, target) == False:
     print("Элемент не найден")
 else:
     print(binary_search(num, target))
