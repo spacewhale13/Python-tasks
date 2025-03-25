277db8a1a35d93b923b72252e1c8de401e4afea7
spacewhale13 <alexandranikushina@gmail.com>
2025-03-25 19:53:33 +0300

    Добавлена задача

--- homework_2/quadr.py
+++ homework_2/quadr.py
@@ -1,8 +1,7 @@
-from math import *
+import math
 
 a, b, c = float(input()), float(input()), float(input())
 D = (b**2) - 4 * a * c
-D_sqrt = sqrt(D)
 if a == 0:
     if b != 0:
         print(-c / b)
@@ -11,4 +10,10 @@ elif D < 0:
 elif D == 0:
     print(-b / (2 * a))
 elif D > 0:
-    print((-b - D_sqrt) / (2 * a), (-b + D_sqrt) / (2 * a), sep="\n")
+    Ds = math.sqrt(D)
+    x_1 = (-b - Ds) / (2 * a)
+    x_2 = (-b + Ds) / (2 * a)
+    if x_1 > x_2:
+        print(x_2, x_1, sep="\n")
+    else:
+        print(x_1, x_2, sep="\n")
